5850a835762e1d7acc793fadadfbcceb350486fe
Tyler McKenney <tylermckenney7@gmail.com>
2023-11-02 17:49:28 -0500

    Modify Spring Boot app to include new REST repository query method for searching by category id

--- backend/spring-boot-ecommerce/src/main/java/com/onlinestore/ecommerce/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/onlinestore/ecommerce/dao/ProductRepository.java
@@ -5,9 +5,15 @@ package com.onlinestore.ecommerce.dao;
 */
 
 import com.onlinestore.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200") // Accept calls from web browser scripts for this origin
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // Add query method - performs SQL query: SELECT * FROM product WHERE category_id=?
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
