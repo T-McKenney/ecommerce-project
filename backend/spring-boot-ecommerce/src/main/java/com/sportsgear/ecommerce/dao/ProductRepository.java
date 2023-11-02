87827774e75e8bb5f89a05d923e9f846ee794eeb
Tyler McKenney <tylermckenney7@gmail.com>
2023-11-02 13:36:07 -0500

    Adds CrossOrigin support to Spring Boot Application

--- backend/spring-boot-ecommerce/src/main/java/com/sportsgear/ecommerce/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/sportsgear/ecommerce/dao/ProductRepository.java
@@ -5,7 +5,10 @@ package com.sportsgear.ecommerce.dao;
 */
 
 import com.sportsgear.ecommerce.entity.Product;
+import jakarta.persistence.Column;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200") // Accept calls from web browser scripts for this origin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
