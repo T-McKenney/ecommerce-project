3978ca248cb86851c553c5936ec5b4b6ce2fad63
Tyler McKenney <tylermckenney7@gmail.com>
2023-11-02 17:41:56 -0500

    Adds functionality for ProductListComponent to read category id parameter

--- frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductService} from "../../services/product.service";
 import {Product} from "../../common/product";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-product-list',
@@ -10,15 +11,32 @@ import {Product} from "../../common/product";
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
-  constructor(private productService: ProductService) { } // Inject our ProductService
+  currentCategoryId: number = 1;
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { } // Inject our ProductService
 
   ngOnInit(): void {
 
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    })
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(
+
+    // Check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the "id" param string. Convert string to a number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      // no category id available...default to category id: 1
+      this.currentCategoryId = 1;
+    }
+
+    // now get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
