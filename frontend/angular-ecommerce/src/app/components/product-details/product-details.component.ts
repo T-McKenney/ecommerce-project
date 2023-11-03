514f8a73b9df14a79b36affc4c9813466b032130
Tyler McKenney <tylermckenney7@gmail.com>
2023-11-03 16:08:58 -0500

    Adds cart functionality for when inside detail view of a product

--- frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../../services/product.service";
 import {ActivatedRoute} from "@angular/router";
 import {Product} from "../../common/product";
+import {CartItem} from "../../common/cart-item";
+import {CartService} from "../../services/cart.service";
 
 @Component({
   selector: 'app-product-details',
@@ -13,7 +15,8 @@ export class ProductDetailsComponent implements OnInit {
   product!: Product;
 
   constructor(private route: ActivatedRoute,
-              private productService: ProductService) {
+              private productService: ProductService,
+              private cartService: CartService) {
   }
 
   ngOnInit(): void {
@@ -33,5 +36,13 @@ export class ProductDetailsComponent implements OnInit {
         this.product = data;
       }
     )
+  }
+  addToCart() {
+
+    console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`);
+
+    const theCartItem = new CartItem(this.product);
+
+    this.cartService.addToCart(theCartItem);
   }
 }
