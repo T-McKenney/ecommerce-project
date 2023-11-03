45587384d608cd096be7f15b0df142e832546ec7
Tyler McKenney <tylermckenney7@gmail.com>
2023-11-03 11:38:09 -0500

    Adds new compoenent for master/detail view of individual products.
    Adds functionality for accessing view through clicking on picture/product name.
    Displays individual product data upon accessing.

---
+++ frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../../services/product.service";
+import {ActivatedRoute} from "@angular/router";
+import {Product} from "../../common/product";
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  product!: Product;
+
+  constructor(private route: ActivatedRoute,
+              private productService: ProductService) {
+  }
+
+  ngOnInit(): void {
+
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    })
+  }
+
+  private handleProductDetails() {
+
+    //get the "id" param string. convert string to a number using the "+" symbol
+    const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
+
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data;
+      }
+    )
+  }
+}
--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -43,6 +43,14 @@ export class ProductService {
       map(response => response._embedded.products)
     );
   }
+
+  getProduct(theProductId: number) {
+
+    // need to build URL based on product id
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+
+    return this.httpClient.get<Product>(productUrl);
+  }
 }
 
 interface GetResponseProducts {
