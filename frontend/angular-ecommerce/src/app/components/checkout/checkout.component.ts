1e17123141b8ef644382d1e2535eac1311aadc8d
Tyler McKenney <tylermckenney7@gmail.com>
2023-11-04 09:48:23 -0500

    chore: fix 'Review Your Order' not displaying <p>
    adds credit card form section
    adds review your order form section

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
@@ -10,6 +10,9 @@ export class CheckoutComponent implements OnInit {
 
 
   checkoutFormGroup!: FormGroup;
+
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -59,4 +62,5 @@ export class CheckoutComponent implements OnInit {
       this.checkoutFormGroup.controls['billingAddress'].reset();
     }
   }
+
 }
