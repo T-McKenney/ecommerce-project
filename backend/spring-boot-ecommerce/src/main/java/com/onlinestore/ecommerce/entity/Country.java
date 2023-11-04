31cbefab843bf5155a0620935080b907ae435506
Tyler McKenney <tylermckenney7@gmail.com>
2023-11-04 10:54:58 -0500

    Adds Country & State entities to backend

---
+++ backend/spring-boot-ecommerce/src/main/java/com/onlinestore/ecommerce/entity/Country.java
@@ -0,0 +1,32 @@
+package com.onlinestore.ecommerce.entity;
+
+/*
+    Created by tylermckenney on 11/4/23.
+*/
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "country")
+@Getter
+@Setter
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "country")
+    private List<State> states;
+}
---
+++ backend/spring-boot-ecommerce/src/main/java/com/onlinestore/ecommerce/entity/State.java
@@ -0,0 +1,26 @@
+package com.onlinestore.ecommerce.entity;
+
+/*
+    Created by tylermckenney on 11/4/23.
+*/
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "state")
+@Data
+public class State {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+}
