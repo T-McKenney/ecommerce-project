66137905da9433547c80c1cb3c99447a7ba75923
Tyler McKenney <tylermckenney7@gmail.com>
2023-11-04 20:29:02 -0500

    Adds validation for shipping address section of form

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -47,11 +47,22 @@ export class CheckoutComponent implements OnInit {
                                 Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
             }),
             shippingAddress: this.formBuilder.group({
-                street: [''],
-                city: [''],
-                state: [''],
-                country: [''],
-                zipCode: ['']
+                street: new FormControl('',
+                    [Validators.required,
+                        Validators.minLength(2),
+                        FireSaleShopValidators.notOnlyWhitespace]),
+                city: new FormControl('',
+                    [Validators.required,
+                        Validators.minLength(2),
+                        FireSaleShopValidators.notOnlyWhitespace]),
+                state: new FormControl('',
+                    [Validators.required]),
+                country: new FormControl('',
+                    [Validators.required]),
+                zipCode: new FormControl('',
+                    [Validators.required,
+                        Validators.minLength(2),
+                        FireSaleShopValidators.notOnlyWhitespace])
             }),
             billingAddress: this.formBuilder.group({
                 street: [''],
@@ -107,6 +118,26 @@ export class CheckoutComponent implements OnInit {
     }
     get email() {
         return this.checkoutFormGroup.get('customer.email');
+    }
+
+    get shippingAddressStreet() {
+        return this.checkoutFormGroup.get('shippingAddress.street');
+    }
+
+    get shippingAddressCity() {
+        return this.checkoutFormGroup.get('shippingAddress.city');
+    }
+
+    get shippingAddressState() {
+        return this.checkoutFormGroup.get('shippingAddress.state');
+    }
+
+    get shippingAddressCountry() {
+        return this.checkoutFormGroup.get('shippingAddress.country');
+    }
+
+    get shippingAddressZipCode() {
+        return this.checkoutFormGroup.get('shippingAddress.zipCode');
     }
 
     onSubmit() {
