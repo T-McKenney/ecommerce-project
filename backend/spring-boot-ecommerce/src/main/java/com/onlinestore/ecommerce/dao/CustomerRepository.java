1246c2a3c78fbd2dce9a5deb2cfa2fb2f7967e91
Tyler McKenney <tylermckenney7@gmail.com>
2023-11-09 13:58:51 -0600

    Implements backend functionality for checking DB for existing customer email addresses to determine whether or not to create a new entry in customer table.
    If customer exists - attach customer ID to new order
    Else, create new customer entry

--- backend/spring-boot-ecommerce/src/main/java/com/onlinestore/ecommerce/dao/CustomerRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/onlinestore/ecommerce/dao/CustomerRepository.java
@@ -8,4 +8,6 @@ import com.onlinestore.ecommerce.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Customer findByEmail(String theEmail); // performs: SELECT * FROM Customer where customer.email = thEmail
 }
