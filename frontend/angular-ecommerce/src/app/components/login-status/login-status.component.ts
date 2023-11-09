bd12956f3dd34fadc855638c35d78e1d3a5ef5d2
Tyler McKenney <tylermckenney7@gmail.com>
2023-11-09 09:48:32 -0600

    Adds HTML components and CSS styles for webpage implementation of login components

---
+++ frontend/angular-ecommerce/src/app/components/login-status/login-status.component.ts
@@ -0,0 +1,48 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {OKTA_AUTH, OktaAuthStateService} from "@okta/okta-angular";
+import {OktaAuth} from "@okta/okta-auth-js";
+
+@Component({
+  selector: 'app-login-status',
+  templateUrl: './login-status.component.html',
+  styleUrls: ['./login-status.component.css']
+})
+export class LoginStatusComponent implements OnInit {
+
+  isAuthenticated: boolean = false;
+  userFullName: string = '';
+
+  constructor(private oktaAuthService: OktaAuthStateService,
+              @Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {
+  }
+
+  ngOnInit(): void {
+
+    // Subscribe to authentication state changes
+    this.oktaAuthService.authState$.subscribe(
+      (result) => {
+        this.isAuthenticated = result.isAuthenticated!;
+        this.getUserDetails();
+      }
+    )
+  }
+
+  private getUserDetails() {
+    if (this.isAuthenticated) {
+
+      // fetch the logged-in user details (user's claims)
+      this.oktaAuth.getUser().then(
+        (result) => {
+          this.userFullName = result.name as string;
+        }
+      )
+
+    }
+  }
+
+  logout() {
+
+    // terminates the session with Okta and removes current tokens.
+    this.oktaAuth.signOut();
+  }
+}
