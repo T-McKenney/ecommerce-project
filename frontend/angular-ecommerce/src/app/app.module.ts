ed6962cbe7ca6b22529d6e860122c3304bfd13a9
Tyler McKenney <tylermckenney7@gmail.com>
2023-11-09 11:40:10 -0600

    Implements 'member' button & page for authenticated users only
    uses protected route to prevent unauthenticated users from accessing

--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
+import {Injector, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import {HttpClientModule} from "@angular/common/http";
 import {ProductService} from "./services/product.service";
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule, Router} from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
@@ -16,15 +16,27 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import { LoginComponent } from './components/login/login.component';
 import { LoginStatusComponent } from './components/login-status/login-status.component';
-import { OktaAuthModule, OktaCallbackComponent, OKTA_CONFIG } from "@okta/okta-angular";
+import {OktaAuthModule, OktaCallbackComponent, OKTA_CONFIG, OktaAuthGuard} from "@okta/okta-angular";
 import {OktaAuth } from "@okta/okta-auth-js";
 import myAppConfig from "./config/my-app-config";
+import { MembersPageComponent } from './components/members-page/members-page.component';
 
 const oktaConfig = myAppConfig.oidc;
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
+function sendToLoginPage(oktaAuth: OktaAuth, injector: Injector) {
+  // use injector to access any service available within application
+  const router = injector.get(Router);
+
+  // redirect the user to custom login page
+  router.navigate(['/login']);
+}
+
 const routes: Routes = [
+  {path: 'members', component: MembersPageComponent, canActivate: [OktaAuthGuard],
+                    data: {onAuthRequired: sendToLoginPage} },
+
   {path: 'login/callback', component: OktaCallbackComponent}, // once user is authenticated, redirect to the application
   {path: 'login', component: LoginComponent},
 
@@ -49,7 +61,8 @@ const routes: Routes = [
     CartDetailsComponent,
     CheckoutComponent,
     LoginComponent,
-    LoginStatusComponent
+    LoginStatusComponent,
+    MembersPageComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
