8989883570468e9bc881fca8d8a0df506c30dc21
Tyler McKenney <tylermckenney7@gmail.com>
2023-11-10 10:03:08 -0600

    Creates and implements HTTP interceptor
    Places intercepter between frontend and REST API to pass access token in HTTP request to the spring boot backend.
    This places security on the api endpoints for orders, and ensures that only the user that is logged-in has access to the order history of their account.

--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -1,26 +1,27 @@
 import {Injector, NgModule} from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import {HttpClientModule} from "@angular/common/http";
+import {AppComponent} from './app.component';
+import {ProductListComponent} from './components/product-list/product-list.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {ProductService} from "./services/product.service";
 import {Routes, RouterModule, Router} from '@angular/router';
-import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
-import { SearchComponent } from './components/search/search.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { CartStatusComponent } from './components/cart-status/cart-status.component';
-import { CartDetailsComponent } from './components/cart-details/cart-details.component';
-import { CheckoutComponent } from './components/checkout/checkout.component';
+import {ProductCategoryMenuComponent} from './components/product-category-menu/product-category-menu.component';
+import {SearchComponent} from './components/search/search.component';
+import {ProductDetailsComponent} from './components/product-details/product-details.component';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {CartStatusComponent} from './components/cart-status/cart-status.component';
+import {CartDetailsComponent} from './components/cart-details/cart-details.component';
+import {CheckoutComponent} from './components/checkout/checkout.component';
 import {ReactiveFormsModule} from "@angular/forms";
-import { LoginComponent } from './components/login/login.component';
-import { LoginStatusComponent } from './components/login-status/login-status.component';
+import {LoginComponent} from './components/login/login.component';
+import {LoginStatusComponent} from './components/login-status/login-status.component';
 import {OktaAuthModule, OktaCallbackComponent, OKTA_CONFIG, OktaAuthGuard} from "@okta/okta-angular";
-import {OktaAuth } from "@okta/okta-auth-js";
+import {OktaAuth} from "@okta/okta-auth-js";
 import myAppConfig from "./config/my-app-config";
-import { MembersPageComponent } from './components/members-page/members-page.component';
-import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import {MembersPageComponent} from './components/members-page/members-page.component';
+import {OrderHistoryComponent} from './components/order-history/order-history.component';
+import {AuthInterceptorService} from "./services/auth-interceptor.service";
 
 const oktaConfig = myAppConfig.oidc;
 
@@ -35,11 +36,15 @@ function sendToLoginPage(oktaAuth: OktaAuth, injector: Injector) {
 }
 
 const routes: Routes = [
-  {path: 'order-history', component: OrderHistoryComponent, canActivate: [OktaAuthGuard],
-    data: {onAuthRequired: sendToLoginPage} },
+  {
+    path: 'order-history', component: OrderHistoryComponent, canActivate: [OktaAuthGuard],
+    data: {onAuthRequired: sendToLoginPage}
+  },
 
-  {path: 'members', component: MembersPageComponent, canActivate: [OktaAuthGuard],
-                    data: {onAuthRequired: sendToLoginPage} },
+  {
+    path: 'members', component: MembersPageComponent, canActivate: [OktaAuthGuard],
+    data: {onAuthRequired: sendToLoginPage}
+  },
 
   {path: 'login/callback', component: OktaCallbackComponent}, // once user is authenticated, redirect to the application
   {path: 'login', component: LoginComponent},
@@ -54,6 +59,7 @@ const routes: Routes = [
   {path: '', redirectTo: '/products', pathMatch: 'full'},
   {path: '**', redirectTo: '/products', pathMatch: 'full'}
 ]
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -77,7 +83,9 @@ const routes: Routes = [
     ReactiveFormsModule,
     OktaAuthModule
   ],
-  providers: [ProductService, { provide: OKTA_CONFIG, useValue: { oktaAuth }}],
+  providers: [ProductService, {provide: OKTA_CONFIG, useValue: {oktaAuth}},
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
